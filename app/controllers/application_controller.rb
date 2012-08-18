8c490a2b9e1013258b9e889ea5ba8f2b50ca8d9f
Levi Cook <levicook@gmail.com>
2012-08-18 13:00:11 -0600

    Autologin a fake user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :autologin
+
+  helper_method :current_user
+
+  protected
+
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  private
+
+  def autologin
+    unless session.key?(:user_id)
+      fn = Faker::Name.first_name
+      ln = Faker::Name.last_name
+      em = Faker::Internet.email(fn)
+
+      user = User.create!({
+        first_name: fn,
+        last_name: ln,
+        email: em
+      })
+      session[:user_id] = user.id
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User
   include MongoMapper::Document
 
-  key :first_name, String
-  key :last_name, String
-  key :email, String
+  key :first_name , String
+  key :last_name  , String
+  key :email      , String
 
   has_many :beacons
 
