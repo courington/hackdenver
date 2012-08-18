3649c94ff8a5ba9b3fcc0fee94f62b5e13f33b97
Dan Logan <dlogan21@gmail.com>
2012-08-18 12:29:18 -0600

    BACON!!!!!!!!

--- app/controllers/beacons_controller.rb
+++ app/controllers/beacons_controller.rb
@@ -1,15 +1,8 @@
 class BeaconsController < ApplicationController
 
   def index
-    beacons = [
-      { id: 1, lat: 39.743943, lng: -105.020089, user: { name: "Hank"  } },
-      { id: 2, lat: 39.753943, lng: -105.020089, user: { name: "Bill"  } },
-      { id: 3, lat: 39.763943, lng: -105.020089, user: { name: "Bob"   } },
-      { id: 4, lat: 39.773943, lng: -105.020089, user: { name: "Joe"   } },
-      { id: 5, lat: 39.783943, lng: -105.020089, user: { name: "Frank" } },
-    ]
-
-    render json: beacons
+    beacons = Beacon.all
+    render json: beacons, :content_type => 'application/json'
   end
 
 end
