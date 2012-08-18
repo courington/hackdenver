7c4b811ba82e262144c5811df50d77a9885f820d
Dan Logan <dlogan21@gmail.com>
2012-08-18 14:18:25 -0600

    login stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,13 +8,9 @@ class SessionsController < ApplicationController
 
   def create
     if not params[:session].blank?
-      if not User.exists?(['email = ?', params[:session][:email]])
-        user = User.create(:email => params[:session][:email])
-        session[:user_id] = user.id
-        redirect_to root_path
-      else
-        redirect_to :action => :new
-      end
+      user = User.create(:email => params[:session][:email])
+      session[:user_id] = user.id
+      redirect_to root_path
     end
   end
 
