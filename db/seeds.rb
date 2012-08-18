69fb591c70ff1c82f04813d55d1d87d4050aea09
Cory LaNou <cory@joinlocal.com>
2012-08-18 12:18:52 -0600

    models and seeds again?

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Beacon.destroy_all
+User.destroy_all
+
+Beacon.create!(
+  [
+    { user: User.create!({ email: 'dlogan21@gmail.com', first_name: 'Dan', last_name: 'Logan'}),
+      lat: '39.762459',
+      long: '-104.980738',
+      description: 'Coding some GO @ Novo Cofee',
+      duration: 2
+    },
+    { user: User.create!({ email: 'levicook@gmail.com', first_name: 'Levi', last_name: 'Cook'}),
+      lat: '39.761055',
+      long: '-104.982477',
+      description: 'General Hacking, go/ruby/whatev @ Crema Coffee House',
+      duration: 1
+    },
+   { user: User.create!({ email: 'cory@lanou.com', first_name: 'Cory', last_name: 'LaNou'}),
+      lat: '39.750972',
+      long: '-104.987671',
+      description: 'Hacking up some ruby code with facebook apis @ Flying Dog Brewery',
+      duration: 4
+    }
+  ]
+)
