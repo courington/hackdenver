228ea329e36d60f322d17cb8b4e40ca41e0ab45f
Dan Logan <dlogan21@gmail.com>
2012-08-18 16:38:54 -0600

    logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Hd::Application.routes.draw do
   resources :sessions
   match 'login', :to => 'sessions#new'
   match 'beacons/edit', :to => 'beacons#edit'
+
+  match 'logout', :to => 'sessions#logout'
   root to: 'welcome#home'
 
 end
