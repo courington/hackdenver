9128ccd43491000ab4c9c4731f42c22d5be24800
Cory LaNou <cory@joinlocal.com>
2012-08-18 12:36:24 -0600

    beacon#show

--- app/controllers/beacons_controller.rb
+++ app/controllers/beacons_controller.rb
@@ -1,15 +1,15 @@
 class BeaconsController < ApplicationController
 
   def index
-    beacons = [
-      { id: 1, lat: 39.743943, lng: -105.020089, user: { name: "Hank"  } },
-      { id: 2, lat: 39.753943, lng: -105.020089, user: { name: "Bill"  } },
-      { id: 3, lat: 39.763943, lng: -105.020089, user: { name: "Bob"   } },
-      { id: 4, lat: 39.773943, lng: -105.020089, user: { name: "Joe"   } },
-      { id: 5, lat: 39.783943, lng: -105.020089, user: { name: "Frank" } },
-    ]
-
-    render json: beacons
+    render json: Beacon.all.to_json
   end
 
+  def show
+    beacon = Beacon.find(params[:id])
+    json = {
+      beacon: beacon.attributes.to_hash.slice('lat', 'lng', 'description', 'duration'),
+      user:  beacon.user
+    }
+    render json: json.to_json
+  end
 end
